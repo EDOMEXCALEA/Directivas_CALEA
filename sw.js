712f51018e467366f16620ba301034d77e894045
EDOMEXCALEA <subsecretariapoliciaedomex@gmail.com>
2026-03-23 17:14:43 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'calea-pwa-v1';
+const urlsToCache = [
+  '/Directivas_CALEA/',
+  '/Directivas_CALEA/index.html',
+  '/Directivas_CALEA/pdfjs.json',
+  '/Directivas_CALEA/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
